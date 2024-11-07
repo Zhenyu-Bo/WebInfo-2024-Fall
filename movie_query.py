a2fd5803724decfc5172cf5143578240c79af121
Zhenyu-Bo <bzy1117@mail.ustc.edu.cn>
2024-11-07 10:24:08 +0800

    modify file structure

--- movie_query.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author: Zhenyu Bo
-# Date: 2024-11-06 22:47:58
-# LastEditor: Zhenyu Bo
-# LastEditTime: 2024-11-06 23:14:24
-
---
+++ src/stage1/gen_inverted_list.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Author: Zhenyu Bo
+# Date: 2024-11-06
+
