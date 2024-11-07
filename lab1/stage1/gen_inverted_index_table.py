fc7dcaf41f90aa6c60432b3441d66608cef01e38
Zhenyu-Bo <bzy1117@mail.ustc.edu.cn>
2024-11-07 13:26:01 +0800

    modify gen_inverted_index_table

--- lab1/stage1/gen_inverted_index_table.py
+++ lab1/stage1/gen_inverted_index_table.py
@@ -2,36 +2,29 @@
 # Author: Zhenyu Bo
 # Date: 2024-11-06
 
-import pandas as pd
 import ast
+import pandas as pd
 
 
-def read_tags_from_csv(file_path, is_book):
+def read_words_from_csv(file_path):
     """
     读取 CSV 文件，提取所有单词和文档内容。
 
     参数：
     - file_path: CSV 文件路径。
-    - is_book: 是否为书籍数据。
 
     返回：
-    - all_words: 集合，包含所有词项集合。
-    - documents: 字典，键为文档 ID，值为该文档的标签。
+    - all_words: 所有词项的集合。
+    - documents: 字典，键为文档 ID，值为该文档包含的单词集合。
     """
-    if is_book:
-        dtype = {'Book': int, 'Tags': str}
-        first_col = 'Book'
-    else:
-        dtype = {'Movie': int, 'Tags': str}
-        first_col = 'Movie'
-    data = pd.read_csv(file_path, dtype=dtype)
+    data = pd.read_csv(file_path, dtype={'id': int, 'words': str})
     all_words = set()
     documents = {}
     for idx in range(len(data)):
-        tags = ast.literal_eval(data.at[idx, 'Tags'])
-        doc_id = data.at[idx, first_col]
-        documents[doc_id] = tags
-        all_words.update(tags)
+        words = ast.literal_eval(data.at[idx, 'words'])
+        doc_id = data.at[idx, 'id']
+        documents[doc_id] = words
+        all_words.update(words)
     return all_words, documents
 
 
@@ -40,7 +33,7 @@ def generate_inverted_index_table(all_words, documents, output_file):
     生成倒排索引表并保存为 CSV 文件。
 
     参数：
-    - all_tags: 集合，包含所有标签。
+    - all_words: 集合，包含所有词项。
     - documents: 字典，键为文档 ID，值为该文档的标签集合。
     - output_file: 输出的 CSV 文件路径。
     """
@@ -62,12 +55,12 @@ def generate_inverted_index_table(all_words, documents, output_file):
 
 def main():
     # 处理书籍数据
-    all_book_words, book_documents = read_tags_from_csv("../data/selected_book_top_1200_data_tag.csv", True)
-    generate_inverted_index_table(all_book_words, book_documents, "../data/book_inverted_list.csv")
+    all_book_words, book_documents = read_words_from_csv("data/book_words.csv")
+    generate_inverted_index_table(all_book_words, book_documents, "data/book_inverted_index_table.csv")
 
     # 处理电影数据
-    all_movie_words, movie_documents = read_tags_from_csv("data/selected_movie_top_1200_data_tag.csv", False)
-    generate_inverted_index_table(all_movie_words, movie_documents, "../data/movie_inverted_list.csv")
+    all_movie_words, movie_documents = read_words_from_csv("data/movie_words.csv")
+    generate_inverted_index_table(all_movie_words, movie_documents, "data/movie_inverted_index_table.csv")
 
 
 if __name__ == "__main__":
