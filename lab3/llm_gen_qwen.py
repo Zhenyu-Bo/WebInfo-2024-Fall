97e0e8f2a23eb84e2fe176ea34f62cd24a3a5f5e
gdh <guodonghao@mail.ustc.edu.cn>
2025-01-12 23:30:11 +0800

    优化输出界面

--- lab3/llm_gen_qwen.py
+++ lab3/llm_gen_qwen.py
@@ -1,13 +1,17 @@
 from openai import OpenAI
-from langchain.prompts import ChatPromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 GDH_API_KEY = "sk-66ccd9858bc24cce93e1b5f9ae542262"
 
-def rag_answer(query: str, vectorstore):
+# 加载向量化模型和FAISS索引到内存中
+embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+faiss_index_path = "faiss_index_2"
+vectorstore = FAISS.load_local(faiss_index_path, embeddings, allow_dangerous_deserialization=True)
+
+def rag_answer(query: str):
     # 1. 检索相关文档
-    retrieved_docs = vectorstore.similarity_search(query, k=5)
+    retrieved_docs = vectorstore.similarity_search(query, k=10)
 
     # 2. 构造上下文文本
     context_str = "\n".join([doc.page_content.replace("data: ", "") for doc in retrieved_docs])
@@ -28,23 +32,19 @@ def rag_answer(query: str, vectorstore):
                 {'role': 'user', 'content': query}
             ]
         )
-        print("=================Context===================")
-        print("System: 你是专业的法律知识问答助手。你需要使用以下检索到的上下文片段来回答问题，检索到的上下文如下：", )
-        print(context_str)
-        print("System: 你只能使用上述上下文来回答下面的问题。如果如果上下文中没有足够依据，直接回答“未找到相关答案”。")
         print("=================QA===================")
         print("User:", query)
         res = completion.choices[0].message.content
+        print("Assistant:", res)
+        print()
+        print()
     except Exception as e:
         print(f"错误信息：{e}")
         print("请参考文档：https://help.aliyun.com/zh/model-studio/developer-reference/error-code")
         res = "ERROR"
 
-    return res
-
 # 示例调用
 if __name__ == "__main__":
-    faiss_index_path = "faiss_index_2"
     questions = ["借款人去世，继承人是否应履行偿还义务？",
                  "如何通过法律手段应对民间借贷纠纷？",
                  "没有赡养老人就无法继承财产吗？",
@@ -62,12 +62,5 @@ if __name__ == "__main__":
                     上述犯罪事实。经鉴定，被告人 xxx 被诊断为精神分裂症，限制刑事责
                     任能力，有受审能力。''',
                 "你现在是一个精通中国法律的法官，请对以下案件做出分析：2012 年 5月 1 日，原告 xxx 在被告 xxxx 购买“玉兔牌”香肠 15 包，其中价值 558.6 元的 14 包香肠已过保质期。xxx 到收银台结账后，即径直到服务台索赔，后因协商未果诉至法院，要求 xxxx 店支付 14 包香肠售价十倍的赔偿金 5586 元。"]
-    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5")
-    vectorstore = FAISS.load_local(faiss_index_path, embeddings, allow_dangerous_deserialization=True)
     for question in questions:
-        print("=================NEW QUESTION===================")
-        print(f"本次问题：{question}")
-        answer = rag_answer(question, vectorstore)
-        print("Assistant:", answer)
-        print()
-        print()
+        rag_answer(question)
